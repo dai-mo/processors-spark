2304a888d21dfa7881bca550ab7ed1a71cb7f2ee
Cherian Mathew <cmathew@chakravyu.org>
2015-12-28 22:58:05 +0100

    #7 Add Annotation based Declarative Services Servlet Example

--- org.dcs.servlet.basic/src/main/java/org/dcs/servlet/basic/TestServlet.java
+++ org.dcs.servlet.basic/src/main/java/org/dcs/servlet/basic/TestServlet.java
@@ -16,7 +16,7 @@ import java.io.PrintWriter;
 public class TestServlet extends HttpServlet {
 
 
-    public static final String SERVLET_ALIAS = "/test";
+    public static final String SERVLET_ALIAS = "/basic";
 
     private static final long serialVersionUID = 1L;
 
@@ -45,6 +45,7 @@ public class TestServlet extends HttpServlet {
 
         out.println("Request = " + req);
         out.println("PathInfo = " + req.getPathInfo());
+        out.println("Big Hello From the Activator Servlet ");
     }
 }
 
---
+++ org.dcs.servlet.ds/src/main/java/org/dcs/servlet/ds/TestServletDS.java
@@ -0,0 +1,86 @@
+package org.dcs.servlet.ds;
+
+import org.osgi.service.component.annotations.*;
+import org.osgi.service.http.HttpService;
+import org.osgi.service.http.NamespaceException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by cmathew on 28/12/15.
+ */
+
+@Component(
+        name = "org.dcs.servlet.ds.TestServletDS",
+        immediate = true
+)
+public class TestServletDS extends HttpServlet {
+
+
+    public static final String SERVLET_ALIAS = "/ds";
+
+    private static final long serialVersionUID = 1L;
+
+    private HttpService httpService;
+
+    public TestServletDS() {
+    }
+
+    @Activate
+    protected void start() {
+
+        try {
+
+            httpService.registerServlet(SERVLET_ALIAS, this, null,
+                    httpService.createDefaultHttpContext());
+        } catch (ServletException | NamespaceException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException
+    {
+        res.setContentType("text/plain");
+        PrintWriter out = res.getWriter();
+
+        out.println("Request = " + req);
+        out.println("PathInfo = " + req.getPathInfo());
+        out.println("Big Hello From the Declarative Services (Annotation) Servlet");
+    }
+
+    @Reference(
+            name = "http.service",
+            service = HttpService.class,
+            cardinality = ReferenceCardinality.MANDATORY,
+            policy = ReferencePolicy.STATIC,
+            unbind = "unsetHttpService"
+    )
+    protected void setHttpService(HttpService httpService) {
+        this.httpService = httpService;
+    }
+
+    protected void unsetHttpService(HttpService httpService) {
+        this.httpService = null;
+    }
+}
+
+
