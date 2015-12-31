488043d2c59a9cc0319f9f9474b4e6fcc15449b7
Cherian Mathew <cmathew@chakravyu.org>
2015-12-31 16:09:10 +0100

    #8 Add DCS triple view initial commit

---
+++ org.dcs.web.ui/app/dcs.js
@@ -0,0 +1,10 @@
+/**
+ * Created by cmathew on 31/12/15.
+ */
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('dcs', [
+    'myApp.version',
+    'angularResizable'
+])
