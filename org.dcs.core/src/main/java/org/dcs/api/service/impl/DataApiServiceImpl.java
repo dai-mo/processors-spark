c2ad086eed129cc7cc4d4f8da024916a175e6779
Cherian Mathew <cmathew@chakravyu.org>
2016-01-31 15:42:41 +0100

    Add dependencies for pax cdi + weld osgi in web project

--- org.dcs.core/src/main/java/org/dcs/api/service/impl/DataApiServiceImpl.java
+++ org.dcs.core/src/main/java/org/dcs/api/service/impl/DataApiServiceImpl.java
@@ -5,6 +5,7 @@ import org.dcs.api.data.DataManagerException;
 import org.dcs.api.model.DataLoader;
 import org.dcs.api.service.DataApiService;
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.ops4j.pax.cdi.api.OsgiServiceProvider;
 import org.osgi.service.component.annotations.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,7 +18,7 @@ import java.util.UUID;
 
 @javax.annotation.Generated(value = "class io.swagger.codegen.languages.JaxRSServerCodegen", date = "2016-01-26T15:01:10.051+01:00")
 @Component
-//@OsgiServiceProvider
+@OsgiServiceProvider
 public class DataApiServiceImpl implements DataApiService {
   static final Logger logger = LoggerFactory.getLogger(DataApiServiceImpl.class);
 
