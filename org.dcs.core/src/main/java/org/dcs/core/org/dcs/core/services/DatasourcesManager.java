76bd9446971778173c8c8b05f6bd23d66f9da710
Laurent B <lbnetid+gh@gmail.com>
2016-03-04 14:00:27 +0100

    Added logging

--- org.dcs.core/src/main/java/org/dcs/core/org/dcs/core/services/DatasourcesManager.java
+++ org.dcs.core/src/main/java/org/dcs/core/org/dcs/core/services/DatasourcesManager.java
@@ -9,6 +9,8 @@ import org.dcs.api.service.RESTException;
 import org.ops4j.pax.cdi.api.OsgiService;
 import org.osgi.service.cm.Configuration;
 import org.osgi.service.cm.ConfigurationAdmin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import java.io.IOException;
@@ -22,10 +24,11 @@ import java.util.List;
 
 public class DatasourcesManager {
 
+  private static final Logger logger = LoggerFactory.getLogger(DatasourcesManager.class);
 
   public static final String ZOOKEEPER_CONFIG_STRING = "zookeeper.hosts";
   public static final String ZOOKEEPER_DATASOURCE_PATH = "zookeeper.datasources";
-  private static final String CONFIG_ID = "org.dcs";
+  public static final String CONFIG_ID = "org.dcs";
 
   @Inject
   @OsgiService
@@ -38,7 +41,7 @@ public class DatasourcesManager {
   public DatasourcesManager() throws RESTException {
     retryPolicy = new ExponentialBackoffRetry(1000, 3);
     connectionString = (String) configuration().getProperties().get(ZOOKEEPER_CONFIG_STRING);
-    baseDatasourcesPath = (String)(configuration().getProperties().get(ZOOKEEPER_DATASOURCE_PATH));
+    baseDatasourcesPath = (String) configuration().getProperties().get(ZOOKEEPER_DATASOURCE_PATH);
   }
 
   public DatasourcesManager addDatasource(String sourceName, String uriStr) throws RESTException {
@@ -47,8 +50,10 @@ public class DatasourcesManager {
       path.append("/").append(sourceName);
       fwk.start();
       fwk.create().forPath(baseDatasourcesPath, uriStr.getBytes());
+      logger.debug(new StringBuilder("Added datasource: ").append(sourceName).toString());
       return this;
     } catch (Exception e) {
+      logger.error(new StringBuilder("Cannot add datasource: ").append(sourceName).toString());
       throw new RESTException(ErrorConstants.getErrorResponse("DCS101"), e);
     }
   }
@@ -68,6 +73,7 @@ public class DatasourcesManager {
     try {
       return configAdmin.getConfiguration(CONFIG_ID);
     } catch (IOException e) {
+      logger.error(new StringBuilder("Cannot get configuration for ").append(CONFIG_ID).toString());
       throw new RESTException(ErrorConstants.getErrorResponse("DCS101"), e);
     }
   }
