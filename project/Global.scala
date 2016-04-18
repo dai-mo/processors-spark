8f7a218568c6a326ab9aee0bf122ecafac5804c6
Cherian Mathew <cmathew@chakravyu.org>
2016-04-18 11:41:38 +0200

    #47: moving to new artifactory site

--- project/Global.scala
+++ project/Global.scala
@@ -1,19 +1,6 @@
 import sbt._
 
 object Global {
-  // Versions
-  lazy val scVersion = "2.11.8"
-  lazy val nanonetArtifactoryBaseUrl = "http://artifactory.openshift.nanonet"
-
-  // Repositories
-  val localMavenRepository = (
-    "Local Maven Repository" at "file://" +
-    Path.userHome.absolutePath +
-    "/.m2/repository")
-
-  val nanonetMavenRepository = (
-        "Nanonet Maven Repository" at nanonetArtifactoryBaseUrl + "/artifactory/libs-snapshot-local/"
-    )
 
   val paxCdiCapabilities = "org.ops4j.pax.cdi.extension;" +
     "filter:=\"(&(extension=pax-cdi-extension)(version>=0.12.0)(!(version>=1.0.0)))\"," +
