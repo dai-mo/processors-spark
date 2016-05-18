73530e7bdf0731f8dc1720c95b859c26bfe2fa6c
Cherian Mathew <cmathew@chakravyu.org>
2016-05-18 09:02:26 +0200

    Add ui config service impl

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,6 +22,7 @@ object Dependencies {
 			val dcsData         = "org.dcs"                    % "org.dcs.data"            % dcsVersion
 			val dcsApi          = "org.dcs"                    % "org.dcs.api"             % dcsVersion
 			val dcsTest         = "org.dcs"                    % "org.dcs.test"            % dcsVersion
+			val dcsCommons      = "org.dcs"                    % "org.dcs.commons"         % dcsVersion
 			val paxCdiApi       = "org.ops4j.pax.cdi"          % "pax-cdi-api"             % paxCdiVersion
 			val cdiApi          = "javax.enterprise"           % "cdi-api"                 % cdiApiVersion
 			val logbackCore     = "ch.qos.logback"             % "logback-core"            % logbackVersion
@@ -47,7 +48,7 @@ object Dependencies {
 					logbackCore     % "provided",
 					logbackClassic  % "provided",
 					curator                       exclude("log4j", "log4j") exclude("org.slf4j", "slf4j-api"),
-					zookeeper                     exclude("log4j", "log4j") exclude("org.slf4j", "slf4j-api") exclude("org.slf4j", "slf4j-log4j12"),					
+					zookeeper                     exclude("log4j", "log4j") exclude("org.slf4j", "slf4j-api") exclude("org.slf4j", "slf4j-log4j12"),
 					scalaReflect,
 
 					dcsTest         % "test",
