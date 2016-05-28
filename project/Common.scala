82d11be5e9d54dedba82cab64919358aad73f43b
Cherian Mathew <cmathew@chakravyu.org>
2016-05-28 18:16:24 +0200

    Fix for idea

--- project/Common.scala
+++ project/Common.scala
@@ -28,6 +28,13 @@ object Common {
     javacOptions in doc := Seq("-source", "1.8")
     )
 
+  lazy val paxCdiCapabilities = "org.ops4j.pax.cdi.extension;" +
+      "filter:=\"(&(extension=pax-cdi-extension)(version>=0.12.0)(!(version>=1.0.0)))\"," +
+      "osgi.extender;" +
+      "filter:=\"(osgi.extender=pax.cdi)\"," +
+      "org.ops4j.pax.cdi.extension;" +
+      "filter:=\"(extension=pax-cdi-extension)\""
+
   def OsgiProject(projectID: String, projectName: String) = (
     Project(projectID, file(projectName)).
     enablePlugins(SbtOsgi).
