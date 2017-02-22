46cdb26fa73afc69bd7a03c1affa798c36d78ef5
Cherian Mathew <cmathew@chakravyu.org>
2017-02-22 18:41:16 +0100

    Set Int. test to ignore

--- org.dcs.data/src/test/scala/org/dcs/data/DbMigrationSpec.scala
+++ org.dcs.data/src/test/scala/org/dcs/data/DbMigrationSpec.scala
@@ -1,8 +1,11 @@
 package org.dcs.data
 
+import org.scalatest.Ignore
+
 /**
   * Created by cmathew on 17.02.17.
   */
+@Ignore
 class DbMigrationSpec extends DbMigrationBehaviour {
 
   "Database migration" should " update database according to schema changes" in {
