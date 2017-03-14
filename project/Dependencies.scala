4f2a8615f73f54192e04167588a596bc3da4bf19
Cherian Mathew <cmathew@chakravyu.org>
2017-03-14 10:31:51 +0100

    AL-149 : Update deps. for release

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,9 +3,9 @@ import sbt._
 object Dependencies {
 	// Versions
 	lazy val scVersion             = "2.11.7"
-	lazy val dcsApiVersion    		 = "0.3.0-SNAPSHOT"
-	lazy val dcsCommonsVersion     = "0.2.0-SNAPSHOT"
-	lazy val dcsDataVersion        = "0.2.0-SNAPSHOT"
+	lazy val dcsApiVersion    		 = "0.3.0"
+	lazy val dcsCommonsVersion     = "0.2.0"
+	lazy val dcsDataVersion        = "0.2.0"
 	lazy val dcsTestVersion   		 = "0.1.0"
 	lazy val paxCdiVersion    		 = "0.12.0"
 	lazy val cdiApiVersion    		 = "1.2"
@@ -110,4 +110,3 @@ object Dependencies {
 	)
 
 }
-
