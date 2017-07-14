2c80e766a1cf6fde2b686e3d9f89c7a743c79eeb
Cherian Mathew <cmathew@chakravyu.org>
2017-07-14 13:47:37 +0200

    Correct gbif processor to handle

--- org.dcs.core/src/main/scala/org/dcs/core/processor/GBIFOccurrenceProcessor.scala
+++ org.dcs.core/src/main/scala/org/dcs/core/processor/GBIFOccurrenceProcessor.scala
@@ -53,47 +53,59 @@ class GBIFOccurrenceProcessor extends StatefulRemoteProcessor
   override def execute(record: Option[GenericRecord], propertyValues: util.Map[String, String]): List[Either[ErrorResponse, (String, GenericRecord)]] = {
     val species = propertyValue(SpeciesNameProperty, propertyValues)
 
-    val json: util.LinkedHashMap[String, AnyRef] = Json.parseJson(Await.result(
-      getAsEither(path = "/search",
+    val gbifResponse =  Await.result(
+      getAsEither(path = "/searc",
         queryParams = List(
           ("scientificName", species),
           ("offset", offset.toString),
           ("limit", limit.toString))
 
-      ), 20.seconds).right.get.readEntity(classOf[String])).asInstanceOf[util.LinkedHashMap[String, AnyRef]]
-    offset = json.get("offset").asInstanceOf[Int]
-    endOfRecords = json.get("endOfRecords").asInstanceOf[Boolean]
-    val count = json.get("count").asInstanceOf[Int]
-
-    noOfApiCalls = noOfApiCalls + 1
-
-    if(count > 200000)
-      List(Left(ErrorResponse(ErrorConstants.GlobalFlowErrorCode,"Invalid Request", "Occurrence record count exceeds download limit (200000)")))
-    else if(endOfRecords || noOfApiCalls > 1)
-      List()
+      ), 20.seconds)
+    
+    if(gbifResponse.isLeft)
+      List(Left(ErrorResponse(gbifResponse.left.get.code, gbifResponse.left.get.message, gbifResponse.left.get.description)))
     else
-      json.get("results").asInstanceOf[util.List[util.LinkedHashMap[String, AnyRef]]].asScala.map { value => {
-        val writeSchema = RemoteProcessor.resolveWriteSchema(propertyValues, Some(schemaId)).get
-        val gbifOccurrence = new GenericData.Record(writeSchema)
-        gbifOccurrence.put("scientificName", value.get("scientificName"))
-        gbifOccurrence.put("basisOfRecord", value.get("basisOfRecord"))
-        gbifOccurrence.put("taxonRank", value.get("taxonRank"))
-        gbifOccurrence.put("decimalLongitude", value.get("decimalLongitude"))
-        gbifOccurrence.put("decimalLatitude", value.get("decimalLatitude"))
-        gbifOccurrence.put("institutionCode", value.get("institutionCode"))
+    {
 
-        val classificationSchema = writeSchema.getField("classification").schema()
-        val classification = new GenericData.Record(classificationSchema)
-        classification.put("kingdom", value.get("kingdom"))
-        classification.put("phylum", value.get("phylum"))
-        classification.put("order", value.get("order"))
-        classification.put("family", value.get("family"))
-        classification.put("genus", value.get("genus"))
-        classification.put("species", value.get("species"))
+      val json: util.LinkedHashMap[String, AnyRef] =
+        Json.parseJson(gbifResponse.right.get.readEntity(classOf[String]))
+          .asInstanceOf[util.LinkedHashMap[String, AnyRef]]
 
-        gbifOccurrence.put("classification", classification)
-        Right((Success.id, gbifOccurrence))
-      }}.toList
+      offset = json.get("offset").asInstanceOf[Int]
+      endOfRecords = json.get("endOfRecords").asInstanceOf[Boolean]
+      val count = json.get("count").asInstanceOf[Int]
+
+      noOfApiCalls = noOfApiCalls + 1
+
+      if (count > 200000)
+        List(Left(ErrorResponse(ErrorConstants.GlobalFlowErrorCode, "Invalid Request", "Occurrence record count exceeds download limit (200000)")))
+      else if (endOfRecords || noOfApiCalls > 1)
+        List()
+      else
+        json.get("results").asInstanceOf[util.List[util.LinkedHashMap[String, AnyRef]]].asScala.map { value => {
+          val writeSchema = RemoteProcessor.resolveWriteSchema(propertyValues, Some(schemaId)).get
+          val gbifOccurrence = new GenericData.Record(writeSchema)
+          gbifOccurrence.put("scientificName", value.get("scientificName"))
+          gbifOccurrence.put("basisOfRecord", value.get("basisOfRecord"))
+          gbifOccurrence.put("taxonRank", value.get("taxonRank"))
+          gbifOccurrence.put("decimalLongitude", value.get("decimalLongitude"))
+          gbifOccurrence.put("decimalLatitude", value.get("decimalLatitude"))
+          gbifOccurrence.put("institutionCode", value.get("institutionCode"))
+
+          val classificationSchema = writeSchema.getField("classification").schema()
+          val classification = new GenericData.Record(classificationSchema)
+          classification.put("kingdom", value.get("kingdom"))
+          classification.put("phylum", value.get("phylum"))
+          classification.put("order", value.get("order"))
+          classification.put("family", value.get("family"))
+          classification.put("genus", value.get("genus"))
+          classification.put("species", value.get("species"))
+
+          gbifOccurrence.put("classification", classification)
+          Right((Success.id, gbifOccurrence))
+        }
+        }.toList
+    }
   }
 
 
